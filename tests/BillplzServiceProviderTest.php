33e3909aaa3f57211d4285cf1ebd1d5e8ccacf39
Mior Muhammad Zaki <crynobone@gmail.com>
2018-03-23 21:57:25 +0800

    Add tests.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/BillplzServiceProviderTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Billplz\Laravel\TestCase;
+
+use Billplz\Laravel\Billplz;
+use Billplz\Laravel\BillplzServiceProvider;
+
+class BillplzServiceProviderTest extends TestCase
+{
+    /** @test */
+    public function it_has_proper_signature()
+    {
+        $billplz = new BillplzServiceProvider(null);
+
+        $this->assertTrue($billplz->isDeferred());
+        $this->assertSame(['billplz'], $billplz->provides());
+    }
+
+    /** @test */
+    public function it_provides_the_service()
+    {
+        config(['services.billplz.key' => 'abc']);
+
+        $this->assertInstanceOf('Billplz\Client', $this->app->make('billplz'));
+        $this->assertSame('https://www.billplz.com/api', Billplz::getApiEndpoint());
+        $this->assertSame('abc', Billplz::getApiKey());
+    }
+
+    /** @test */
+    public function it_can_configure_api_version()
+    {
+        config([
+            'services.billplz.key' => 'abc',
+            'services.billplz.version' => 'v4',
+        ]);
+
+        $this->assertSame('v4', Billplz::getApiVersion());
+    }
+
+
+    /** @test */
+    public function it_can_use_sandbox_environment()
+    {
+        config([
+            'services.billplz.key' => 'abc',
+            'services.billplz.sandbox' => true,
+        ]);
+
+        $this->assertSame('https://billplz-staging.herokuapp.com/api', Billplz::getApiEndpoint());
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Billplz\Laravel\TestCase;
+
+use Orchestra\Testbench\TestCase as Testbench;
+
+abstract class TestCase extends Testbench
+{
+    /**
+     * Get package aliases.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return array
+     */
+    protected function getPackageAliases($app)
+    {
+        return [
+            'Billplz' => \Billplz\Laravel\Billplz::class,
+        ];
+    }
+
+    /**
+     * Get package providers.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            \Billplz\Laravel\BillplzServiceProvider::class,
+        ];
+    }
+}
