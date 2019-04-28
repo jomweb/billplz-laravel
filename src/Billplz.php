43d3a561113b18c286f0c39393257202c05dbd92
Mior Muhammad Zaki <crynobone@gmail.com>
2019-04-28 13:16:14 +0800

    Add docblock.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Billplz.php
+++ src/Billplz.php
@@ -4,6 +4,18 @@ namespace Billplz\Laravel;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method \Billplz\Client useSandbox()
+ * @method \Billplz\Contracts\Collection collection(string|null $version = null)
+ * @method \BIllplz\Contracts\OpenCollection openCollection(string|null $version = null)
+ * @method \Billplz\Contracts\Bill bill(string|null $version = null)
+ * @method \Billplz\Contracts\Bill\Transaction transaction(string|null $version = null)
+ * @method \BIllplz\Contracts\Collection\MassPayment massPaymentCollection()
+ * @method \Billplz\Contracts\MassPayment massPayment()
+ * @method \Billplz\Contracts\BankAccount bank(string|null $version = null)
+ *
+ * @see \Billplz\Client
+ */
 class Billplz extends Facade
 {
     /**
