9af5c60370547810c69edaf702bd3a7efda7a732
Mior Muhammad Zaki <crynobone@gmail.com>
2020-04-06 09:24:28 +0800

    Allow new and old sandbox url.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Feature/BillplzServiceProviderTest.php
+++ tests/Feature/BillplzServiceProviderTest.php
@@ -46,6 +46,9 @@ class BillplzServiceProviderTest extends TestCase
             'services.billplz.sandbox' => true,
         ]);
 
-        $this->assertSame('https://billplz-staging.herokuapp.com/api', Billplz::getApiEndpoint());
+        $this->assertTrue(\in_array(Billplz::getApiEndpoint(), [
+            'https://billplz-staging.herokuapp.com/api',
+            'https://www.billplz-sandbox.com/api',
+        ]));
     }
 }
